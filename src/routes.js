c65217712c9204703f8280357cdd2b71a42337ef
남현욱 <37534735+hw0k@users.noreply.github.com>
2019-07-15 10:48:43 +0900

    [ADD] Route Definitions for Main

--- src/routes.js
+++ src/routes.js
@@ -1,3 +1,17 @@
 export default [
-
+  {
+    name: '대시보드',
+    iconType: 'menu',
+    url: '/'
+  },
+  {
+    name: '회원 관리',
+    iconType: 'user',
+    url: '/user'
+  },
+  {
+    name: '백업',
+    iconType: 'save',
+    url: '/backup'
+  }
 ];
