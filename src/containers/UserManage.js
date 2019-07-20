bde34d9a3b3fa0952970c9457029f330f8ab174c
서상희 <tbvjaos510@naver.com>
2019-07-20 20:46:30 +0900

    [FIX] fix eslint error

--- src/containers/UserManage.js
+++ src/containers/UserManage.js
@@ -53,35 +53,35 @@ class UserManage extends React.Component {
         setSelectedKeys, selectedKeys, confirm, clearFilters
       }) => (
         <div style={{ padding: 8 }}>
-            <Input
-              ref={(node) => {
-                this.searchInput = node;
-              }}
-              placeholder={`${title} 검색`}
-              value={selectedKeys[0]}
-              onChange={e =>
-                setSelectedKeys(e.target.value ? [e.target.value] : [])
-              }
-              onPressEnter={() => this.handleSearch(selectedKeys, confirm)}
-              style={{ width: 188, marginBottom: 8, display: 'block' }}
-            />
-            <Button
-              type="primary"
-              onClick={() => this.handleSearch(selectedKeys, confirm)}
-              icon="search"
-              size="small"
-              style={{ width: 90, marginRight: 8 }}
-            >
-              검색
-            </Button>
-            <Button
-              onClick={() => this.handleReset(clearFilters)}
-              size="small"
-              style={{ width: 90 }}
-            >
-              초기화
-            </Button>
-          </div>
+          <Input
+            ref={(node) => {
+              this.searchInput = node;
+            }}
+            placeholder={`${title} 검색`}
+            value={selectedKeys[0]}
+            onChange={e =>
+              setSelectedKeys(e.target.value ? [e.target.value] : [])
+            }
+            onPressEnter={() => this.handleSearch(selectedKeys, confirm)}
+            style={{ width: 188, marginBottom: 8, display: 'block' }}
+          />
+          <Button
+            type="primary"
+            onClick={() => this.handleSearch(selectedKeys, confirm)}
+            icon="search"
+            size="small"
+            style={{ width: 90, marginRight: 8 }}
+          >
+            검색
+          </Button>
+          <Button
+            onClick={() => this.handleReset(clearFilters)}
+            size="small"
+            style={{ width: 90 }}
+          >
+            초기화
+          </Button>
+        </div>
         ),
       filterIcon: filtered => (
         <Icon type="search" style={{ color: filtered ? '#1890ff' : undefined }} />
