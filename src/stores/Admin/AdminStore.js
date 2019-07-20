0b3580a82c161328649d219382a495e1d9cc0dd5
서상희 <tbvjaos510@naver.com>
2019-07-20 17:38:32 +0900

    [FIX] change default jwt token

--- src/stores/Admin/AdminStore.js
+++ src/stores/Admin/AdminStore.js
@@ -15,7 +15,7 @@ class AdminStore {
    * test jwt 토큰 값 (나중에 무조건 지울 것!)
    */
   @observable
-  jwt = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJsdW1lbi1qd3QiLCJzdWIiOjIsImlhdCI6MTU2MzYwODMxOSwiZXhwIjoxNTYzNjExOTE5fQ.gl1990DxieYRZ9cGpyrFODRsdHPG6vaSaWwIp4jlUq0'
+  jwt = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJsdW1lbi1qd3QiLCJzdWIiOjIsImlhdCI6MTU2MzYxMTgyMSwiZXhwIjoxNTY4Nzk1ODIxfQ.YswkBeWysD74s5DPbft9XvdSqmzmF2mopoL4UdLtL10'
 
   @computed
   get isAuthed() {
