ddf408bc1486b06cf0e7bdb36e8d44f76c33a13c
서상희 <tbvjaos510@naver.com>
2019-07-20 16:05:34 +0900

    [ADD] add test jwt

--- src/stores/Admin/AdminStore.js
+++ src/stores/Admin/AdminStore.js
@@ -11,8 +11,11 @@ class AdminStore {
     AdminStore.instance = this;
   }
 
+  /**
+   * test jwt 토큰 값 (나중에 무조건 지울 것!)
+   */
   @observable
-  jwt = ''
+  jwt = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJsdW1lbi1qd3QiLCJzdWIiOjIsImlhdCI6MTU2MzYwNTk5NywiZXhwIjoxNTYzNjA5NTk3fQ.1We3ZtaYmg3vOS7bBbN7FhjOdFloxVpVmrk2ee9fuCY'
 
   @computed
   get isAuthed() {
