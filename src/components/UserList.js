11947778671de9f598209d8d8b9bf25c4f193211
서상희 <tbvjaos510@naver.com>
2019-07-22 14:59:38 +0900

    [ADD] UserList로 별도 추출

---
+++ src/components/UserList.js
@@ -0,0 +1,124 @@
+import React, { useState } from 'react';
+import {
+  Input, Icon, Table, Button
+} from 'antd';
+import Highlighter from 'react-highlight-words';
+import Column from 'antd/lib/table/Column';
+
+const UserList = (props) => {
+  const {
+    isCollapsed, memberList, afterColumns, beforeColumns
+  } = props;
+  let searchInput = null;
+  const [searchText, setSearchText] = useState('');
+  const handleSearch = (selectedKeys, confirm) => {
+    confirm();
+    setSearchText(selectedKeys[0]);
+  };
+
+  const handleReset = (clearFilters) => {
+    clearFilters();
+    setSearchText('');
+  };
+  const getColumnSearchProps = (title, dataIndex) => ({
+    filterDropdown: dropdown => (
+      <div style={{ padding: 8 }}>
+        <Input
+          ref={(node) => {
+            searchInput = node;
+          }}
+          placeholder={`${title} 검색`}
+          value={dropdown.selectedKeys[0]}
+          onChange={e =>
+            dropdown.setSelectedKeys(e.target.value ? [e.target.value] : [])
+          }
+          onPressEnter={() => handleSearch(dropdown.selectedKeys, dropdown.confirm)}
+          style={{ width: 188, marginBottom: 8, display: 'block' }}
+        />
+        <Button
+          type="primary"
+          onClick={() => handleSearch(dropdown.selectedKeys, dropdown.confirm)}
+          icon="search"
+          size="small"
+          style={{ width: 90, marginRight: 8 }}
+        >
+          검색
+        </Button>
+        <Button
+          onClick={() => handleReset(dropdown.clearFilters)}
+          size="small"
+          style={{ width: 90 }}
+        >
+          초기화
+        </Button>
+      </div>
+    ),
+    filterIcon: filtered => (
+      <Icon type="search" style={{ color: filtered ? '#1890ff' : undefined }} />
+    ),
+    onFilter: (value, record) =>
+      record[dataIndex]
+        .toString()
+        .toLowerCase()
+        .includes(value.toLowerCase()),
+    onFilterDropdownVisibleChange: (visible) => {
+      if (visible) {
+        setTimeout(() => searchInput.select());
+      }
+    },
+    render: text => (
+      <Highlighter
+        highlightStyle={{ backgroundColor: '#ffc069', padding: 0 }}
+        searchWords={[searchText]}
+        autoEscape
+        textToHighlight={text.toString()}
+      />
+    )
+  });
+
+
+  return (
+    <Table dataSource={memberList} rowKey={user => user.id}>
+      {
+        beforeColumns
+      }
+      <Column
+        title="이름"
+        dataIndex="name"
+        key="name"
+        {...getColumnSearchProps('이름', 'name')}
+      />
+      {
+        // TODO: Fragment 미동작. 더 좋은 방법을 제시해주세요 ㅠㅠ
+      }
+      {!isCollapsed && (
+        <Column title="생년월일" dataIndex="birth" key="birth" />
+      )}
+      {!isCollapsed && (
+        <Column
+          title="주소"
+          dataIndex="address"
+          key="address"
+          {...getColumnSearchProps('주소', 'address')}
+        />
+      )}
+      {!isCollapsed && (
+        <Column title="등급" dataIndex="level" key="level" />
+      )}
+      {!isCollapsed && (
+        <Column
+          title="전화번호"
+          dataIndex="phone_number"
+          key="phone_number"
+          {...getColumnSearchProps('전화번호', 'phone_number')}
+        />
+      )}
+
+      {
+        afterColumns
+      }
+    </Table>
+  );
+};
+
+export default UserList;
--- src/containers/UserManage.js
+++ src/containers/UserManage.js
@@ -7,22 +7,15 @@ import {
   Divider,
   message,
   Popconfirm,
-  Input,
-  Icon
 } from 'antd';
-import Highlighter from 'react-highlight-words';
-
 import { inject, observer } from 'mobx-react';
+import UserList from '../components/UserList';
 
 const { Column } = Table;
 
 @inject('member', 'layout')
 @observer
 class UserManage extends React.Component {
-  state = {
-    searchText: '',
-  }
-
   addMember = () => {
     const { member } = this.props;
     member.addMember({
@@ -46,133 +39,35 @@ class UserManage extends React.Component {
     }
   };
 
-  getColumnSearchProps = (title, dataIndex) => {
-    const { searchText } = this.state;
-    return ({
-      filterDropdown: ({
-        setSelectedKeys, selectedKeys, confirm, clearFilters
-      }) => (
-        <div style={{ padding: 8 }}>
-          <Input
-            ref={(node) => {
-              this.searchInput = node;
-            }}
-            placeholder={`${title} 검색`}
-            value={selectedKeys[0]}
-            onChange={e =>
-              setSelectedKeys(e.target.value ? [e.target.value] : [])
-            }
-            onPressEnter={() => this.handleSearch(selectedKeys, confirm)}
-            style={{ width: 188, marginBottom: 8, display: 'block' }}
-          />
-          <Button
-            type="primary"
-            onClick={() => this.handleSearch(selectedKeys, confirm)}
-            icon="search"
-            size="small"
-            style={{ width: 90, marginRight: 8 }}
-          >
-            검색
-          </Button>
-          <Button
-            onClick={() => this.handleReset(clearFilters)}
-            size="small"
-            style={{ width: 90 }}
-          >
-            초기화
-          </Button>
-        </div>
-        ),
-      filterIcon: filtered => (
-        <Icon type="search" style={{ color: filtered ? '#1890ff' : undefined }} />
-      ),
-      onFilter: (value, record) =>
-        record[dataIndex]
-          .toString()
-          .toLowerCase()
-          .includes(value.toLowerCase()),
-      onFilterDropdownVisibleChange: (visible) => {
-        if (visible) {
-          setTimeout(() => this.searchInput.select());
-        }
-      },
-      render: text => (
-        <Highlighter
-          highlightStyle={{ backgroundColor: '#ffc069', padding: 0 }}
-          searchWords={[searchText]}
-          autoEscape
-          textToHighlight={text.toString()}
-        />
-      )
-    });
-  };
-
-  handleSearch = (selectedKeys, confirm) => {
-    confirm();
-    this.setState({ searchText: selectedKeys[0] });
-  };
-
-  handleReset = (clearFilters) => {
-    clearFilters();
-    this.setState({ searchText: '' });
-  };
-
   render() {
     const { member, layout } = this.props;
     const { isCollapsed } = layout;
     return (
       <Row type="flex">
         <Col span={24}>
-          <Table dataSource={member.memberList} rowKey={user => user.id}>
-            <Column
-              title="이름"
-              dataIndex="name"
-              key="name"
-              {...this.getColumnSearchProps('이름', 'name')}
-            />
-            {
-              // TODO: Fragment 미동작. 더 좋은 방법을 제시해주세요 ㅠㅠ
-            }
-            {!isCollapsed && (
-              <Column title="생년월일" dataIndex="birth" key="birth" />
-            )}
-            {!isCollapsed && (
+          <UserList
+            memberList={member.memberList}
+            isCollapsed={isCollapsed}
+            afterColumns={(
               <Column
-                title="주소"
-                dataIndex="address"
-                key="address"
-                {...this.getColumnSearchProps('주소', 'address')}
+                key="action"
+                render={user => (
+                  <span>
+                    <a href="#">수정</a>
+                    <Divider type="vertical" />
+                    <Popconfirm
+                      title="정말 삭제하시겠습니까?"
+                      onConfirm={() => this.removeMember(user)}
+                      okText="삭제"
+                      cancelText="취소"
+                    >
+                      <a href="#">삭제</a>
+                    </Popconfirm>
+                  </span>
+                )}
               />
             )}
-            {!isCollapsed && (
-              <Column title="등급" dataIndex="level" key="level" />
-            )}
-            {!isCollapsed && (
-              <Column
-                title="전화번호"
-                dataIndex="phone_number"
-                key="phone_number"
-                {...this.getColumnSearchProps('전화번호', 'phone_number')}
-              />
-            )}
-            <Column
-              key="action"
-              render={user => (
-                <span>
-                  <Button type="link">수정</Button>
-                  <Divider type="vertical" />
-                  <Popconfirm
-                    title="정말 삭제하시겠습니까?"
-                    onConfirm={() => this.removeMember(user)}
-                    okText="삭제"
-                    cancelText="취소"
-                  >
-                    <Button type="link">삭제</Button>
-                  </Popconfirm>
-                </span>
-              )}
-            />
-          </Table>
+          />
         </Col>
         <Col span={24}>
           <Button onClick={this.addMember}>추가</Button>
