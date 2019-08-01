b910883712b5e744a8a60b4bb98cdac511236a35
서상희 <tbvjaos510@naver.com>
2019-08-01 15:45:40 +0900

    [ADD] 메시지 수정

--- src/stores/Member/MemberStore.js
+++ src/stores/Member/MemberStore.js
@@ -59,6 +59,11 @@ class MemberStore {
   }
 
   @computed
+  get memberPhoneNumbers() {
+    return this.memberList.map(m => m.phone_number);
+  }
+
+  @computed
   get regularMemberCount() {
     return this.memberList.filter(m => m.level === '정회원').length;
   }
